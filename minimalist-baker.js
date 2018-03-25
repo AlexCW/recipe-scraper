e9eb4c25640c6adcde142b80c223ae5d9e110c01
Alex Williams <alex.c.williams91@gmail.com>
2018-03-25 13:24:59 +0100

    Bring in moment and extract minutes for prepTime and cookingTime
    Moment durations allows this to happen

--- minimalist-baker.js
+++ minimalist-baker.js
@@ -1,6 +1,8 @@
 let axios = require('axios');
 let cheerio = require('cheerio');
+let moment = require('moment');
 let fs = require('fs');
+
 let entryPoint = 'https://minimalistbaker.com/recipe-index';
 
 var minimalistBaker = {
@@ -25,10 +27,41 @@ var minimalistBaker = {
 
 		return ingredients;
 	},
+	extractMinutesFromText(text) {
+		let mappedTime = text.split(" ").map(function(e, a) {
+			if(a == 0 || a % 2 == 0) {
+				return Number(e);
+			}
+
+			e = e.replace("mins", "minutes");
+			
+			e = e.replace("hour", "hours");
+
+			return e;
+		});
+
+		return Number(moment.duration(...mappedTime).asMinutes());
+	},
+	extractPrepTime (html) {
+		const $ = html;
+
+		let prepTime = $('.ERSTimeItem', '.ERSTimes').find(`[itemprop='prepTime']`).text();
+
+		return this.extractMinutesFromText(prepTime);
+	},
+	extractCookingTime (prepTime, html) {
+		const $ = html;
+
+		let totalTime = $('.ERSTimeItem', '.ERSTimes').find(`[itemprop='totalTime']`).text();
+
+		totalTime = this.extractMinutesFromText(totalTime);
+
+		return totalTime - prepTime;
+	},
 	async resolveRecipe (url) {
 		const timeout = ms => new Promise(res => setTimeout(res, ms));
 
-		await timeout(5000);
+		// await timeout(5000);
 
 		try {
 			const response = await axios.get(url);
@@ -42,6 +75,10 @@ var minimalistBaker = {
 			recipe.title = this.extractTitle(html);
 
 			recipe.ingredients = this.extractIngredients(html);
+			
+			recipe.prepTime = this.extractPrepTime(html);
+
+			recipe.cookingTime = this.extractCookingTime(recipe.prepTime, html);
 
 		} catch (err) {
 			console.log(err)
