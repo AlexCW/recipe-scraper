65e7e6e0bbb3300076b092eea68357dce4b7e650
Alex Williams <alex.c.williams91@gmail.com>
2018-03-25 20:55:03 +0100

    Tidy up + and half working write to json file.

--- minimalist-baker.js
+++ minimalist-baker.js
@@ -7,26 +7,18 @@ let entryPoint = 'https://minimalistbaker.com/recipe-index';
 
 var minimalistBaker = {
 
-	extractTitle (html) {
-		const $ = html;
-
+	extractTitle ($) {
 		let title = $('.entry-title', '.entry-header').text();
-
 		return title;
 	},
-	extractIngredients (html) {
-		const $ = html;
-
+	extractIngredients ($) {
 		let ingredient_container = $('.entry-content').find('.ERSIngredients');
-
 		let ingredients = [];
-
 		$(ingredient_container).each(function(i, container) {
 			$(container).find('.ingredient').each(function(i, ingredient) {
 				ingredients.push($(ingredient).html());
 			});
 		});
-
 		return ingredients;
 	},
 	extractMinutesFromText(text) {
@@ -34,50 +26,30 @@ var minimalistBaker = {
 			if(a == 0 || a % 2 == 0) {
 				return Number(e);
 			}
-
 			e = e.replace("mins", "minutes");
-			
 			e = e.replace("hour", "hours");
-
 			return e;
 		});
-
 		return Number(moment.duration(...mappedTime).asMinutes());
 	},
-	extractPrepTime (html) {
-		const $ = html;
-
+	extractPrepTime ($) {
 		let prepTime = $('.ERSTimeItem', '.ERSTimes').find(`[itemprop='prepTime']`).text();
-
 		return this.extractMinutesFromText(prepTime);
 	},
-	extractCookingTime (prepTime, html) {
-		const $ = html;
-
+	extractCookingTime (prepTime, $) {
 		let totalTime = $('.ERSTimeItem', '.ERSTimes').find(`[itemprop='totalTime']`).text();
-
 		totalTime = this.extractMinutesFromText(totalTime);
-
 		return totalTime - prepTime;
 	},
-	extractTags (html) {
-		const $ = html;
-
+	extractTags ($) {
 		let tagsContainer = $('.divERSHeadItems');
-
 		let typeTags = tagsContainer.find('.ERSCategory').find(`[itemprop='recipeCategory']`).text();
-
 		let cuisineTags = tagsContainer.find('.ERSCuisine').find(`[itemprop='recipeCuisine']`).text();
-
 		return Array.prototype.concat(typeTags.split(","), cuisineTags.split(","));
 	},
-	extractServings (html) {
-		const $ = html;
-
+	extractServings ($) {
 		let tagsContainer = $('.divERSHeadItems');
-
 		let servings = tagsContainer.find('.ERSServes').find(`[itemprop='recipeYield']`).text();
-
 		return Number(servings.replace(/\D/g,''));
 	},
 	async resolveRecipe (url) {
@@ -107,6 +79,9 @@ var minimalistBaker = {
 			recipe.servings = this.extractServings(html);
 
 			recipe.difficulty = 'easy';
+
+			fd = fs.openSync('data/minimalist-baker.json', 'a');
+  			fs.appendFileSync(fd, JSON.stringify(recipe, null, 2), 'utf8');
 
 		} catch (err) {
 			console.log(err)
